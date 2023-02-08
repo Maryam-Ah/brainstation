b1a032b8889432d5ba8db330f613f347735be941
Maryam Ahmadi <43885330+Maryam-Ah@users.noreply.github.com>
2023-02-07 20:02:30 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
 import streamlit as st
+### import libraries
+import pandas as pd
+import streamlit as st
+import joblib
+
+
 
 st.write("hello world")
+
+st.write('Streamlit is an open-source app framework for Machine Learning and Data Science teams. For the docs, please click [here](https://docs.streamlit.io/en/stable/api.html). \
+    This is is just a very small window into its capabilities.')
