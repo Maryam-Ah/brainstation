08cb6b04755cb9bb45287159735612524e28dbb9
Maryam Ahmadi <43885330+Maryam-Ah@users.noreply.github.com>
2023-02-07 19:39:45 -0500

    Update nyc_app_complete.py

--- nyc_app_complete.py
+++ nyc_app_complete.py
@@ -5,8 +5,8 @@ import pandas as pd
 import streamlit as st
 import joblib
 
-st.write('Streamlit is an open-source app framework for Machine Learning and Data Science teams. For the docs, please click [here](https://docs.streamlit.io/en/stable/api.html). \
-    This is is just a very small window into its capabilities.')
+# st.write('Streamlit is an open-source app framework for Machine Learning and Data Science teams. For the docs, please click [here](https://docs.streamlit.io/en/stable/api.html). \
+#     This is is just a very small window into its capabilities.')
 
 
 #######################################################################################################################################
