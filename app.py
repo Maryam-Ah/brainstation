5d01b526b0a4605a27338151ffe5d682fdfa3514
Maryam Ahmadi <43885330+Maryam-Ah@users.noreply.github.com>
2023-02-08 22:25:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,3 +10,20 @@ st.write("hello world")
 
 st.write('Streamlit is an open-source app framework for Machine Learning and Data Science teams. For the docs, please click [here](https://docs.streamlit.io/en/stable/api.html). \
     This is is just a very small window into its capabilities.')
+
+
+### Create a title
+
+st.title("Kickoff - Live coding an app")
+
+# Press R in the app to refresh after changing the code and saving here
+
+### You can try each method by uncommenting each of the lines of code in this section in turn and rerunning the app
+
+# You can also use markdown syntax.
+st.write('# Our last morning kick off :sob:')
+
+### To position text and color, you can use html syntax
+st.markdown("<h1 style='text-align: center; color: blue;'>Our last morning kick off</h1>", unsafe_allow_html=True)
+
+
